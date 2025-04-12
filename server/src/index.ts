75b9db81db530191f6cf1b30376e576d870555a5
guzuro <guzurogmn@gmail.com>
2025-04-12 22:45:02 +0300

    updated drizzle

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,11 @@
-import 'dotenv/config'
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { Elysia } from 'elysia'
+import { node } from '@elysiajs/node'
+import authRoutes from './routes/auth.route'
+import { cors } from '@elysiajs/cors'
 
-export const db = drizzle(process.env.DATABASE_URL!)
+const app = new Elysia({ adapter: node() })
+    .use(cors())
+    .group('/api', (app) => app.use(authRoutes))
+    .listen(3000, ({ hostname, port }) => {
+        console.log(`🦊 Elysia is running at ${hostname}:${port}`)
+    })
---
+++ server/src/models/auth.schema.ts
@@ -0,0 +1,17 @@
+import { t } from 'elysia'
+
+export const signInSchema = t.Object({
+    login: t.String(),
+    password: t.String({ minLength: 6 }),
+})
+
+export type SignInDto = typeof signInSchema.static
+
+export const signUpchema = t.Object({
+    login: t.String(),
+    password: t.String({ minLength: 6 }),
+    passwordConfirm: t.String({ minLength: 6 }),
+})
+
+export type SignUpDto = typeof signUpchema.static
+
