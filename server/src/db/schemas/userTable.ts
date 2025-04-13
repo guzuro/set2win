6cdc987c55cc3c6b38310e42eb64eadaeaab0a6c
guzuro <guzurogmn@gmail.com>
2025-04-13 23:04:55 +0300

    updated db schema

--- server/src/db/schemas/userTable.ts
+++ server/src/db/schemas/userTable.ts
@@ -2,6 +2,6 @@ import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    login: varchar().notNull(),
+    login: varchar().notNull().unique(),
     password: varchar().notNull(),
 })
