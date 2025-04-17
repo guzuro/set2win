dc6bae05e0b65da8fa0bf45cc7c159aedbc6f3ec
guzuro <guzurogmn@gmail.com>
2025-04-17 23:34:27 +0300

    fix forgot await

--- app/src/composables/useAuth.ts
+++ app/src/composables/useAuth.ts
@@ -2,18 +2,23 @@ import { useUserStore } from '@/stores/userStore'
 import type { SignInData, SignUpData } from '@/types/auth.types'
 import { useApi } from './useApi'
 import { AuthApi } from '@/api/auth.api'
+import { useRouter } from 'vue-router'
 
 export default function useAuth() {
     const userStore = useUserStore()
     const { data, error, isLoading, resolve } = useApi()
+    const {push} = useRouter()
 
     async function signIn(payload: SignInData) {
         try {
-            resolve(() => AuthApi.signIn(payload))
+            await resolve(() => AuthApi.signIn(payload))
 
             userStore.$patch({
                 user: data.value,
             })
+
+            push({name: 'Dashboard'})
+            
         } catch (error) {
             console.error(error)
         }
