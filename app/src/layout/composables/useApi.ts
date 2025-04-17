67b8d2c7d1c34029497d25fc6e47d7ff68d09b82
guzuro <guzurogmn@gmail.com>
2025-04-17 22:34:10 +0300

    added useapi composable

---
+++ app/src/layout/composables/useApi.ts
@@ -0,0 +1,29 @@
+import type { AxiosRequestConfig } from 'axios'
+import { ref } from 'vue'
+
+export function useApi<T = any>() {
+    const isLoading = ref(false)
+    const data = ref<T | null>()
+    const error = ref<Error | null>(null)
+
+    async function resolve(reqFn: () => Promise<any>) {
+        isLoading.value = true
+        error.value = null
+        data.value = null
+
+        try {
+            data.value = await reqFn()
+        } catch (error: any) {
+            error.value = error
+        } finally {
+            isLoading.value = false
+        }
+    }
+
+    return {
+        isLoading,
+        data,
+        error,
+        resolve,
+    }
+}
