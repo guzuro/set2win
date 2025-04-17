0da8abb3270471af9d07febd112ffd0c52abba86
guzuro <guzurogmn@gmail.com>
2025-04-17 23:31:03 +0300

    fix

--- app/src/api/auth.api.ts
+++ app/src/api/auth.api.ts
@@ -4,9 +4,9 @@ import type { AxiosRequestConfig } from 'axios'
 
 export const AuthApi = {
     signIn: (params: SignInData, config?: AxiosRequestConfig) => {
-        return api.post('auth/signup', params, config)
+        return api.post('auth/signin', params, config)
     },
     signUp: (params: SignUpData, config?: AxiosRequestConfig) => {
-        return api.post('auth/signin', params, config)
+        return api.post('auth/signup', params, config)
     },
 }
