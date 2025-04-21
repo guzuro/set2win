0f844642cadd5270f8a7dc3864aa668383a60742
guzuro <guzurogmn@gmail.com>
2025-04-21 19:25:32 +0300

    started create player form

---
+++ app/src/modules/players/components/CreatePlayerForm.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="create-player-form">
+        <FormBuilder
+            :form="createPlayerForm"
+            :resolver="createPlayer"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import FormBuilder from '@/shared/components/FormBuilder/FormBuilder.vue'
+import type { FormBuilderData } from '@/shared/components/FormBuilder/types'
+import type { FormResolverOptions } from '@primevue/forms'
+
+const createPlayerForm: FormBuilderData<any> = {
+    model: {
+        fullName: '',
+        country: '',
+        birthDate: '',
+        hand: 'right',
+        favoriteSurface: 'clay',
+    },
+    schema: [
+        {
+          type: 'Container',
+          fields: [
+            {
+                type: 'Text',
+                label: '111',
+                model: 'fullName'
+            },
+            {
+                type: 'Text',
+                label: 'Country',
+                model: 'country'
+            }
+          ]  
+        },
+        {
+            type: 'Text',
+            label: 'hand',
+            model: 'hand    '
+        }
+    ],
+    submit: { label: 'Create player' },
+}
+
+function createPlayer({ values }: FormResolverOptions) {
+    const errors = {}
+    return {
+        values,
+        errors,
+    }
+}
+</script>
+
+<style scoped></style>
--- app/src/modules/players/pages/players-list.vue
+++ app/src/modules/players/pages/players-list.vue
@@ -14,16 +14,33 @@
                     </template>
                 </Card>
             </template>
-            <template v-else> Create your first player! </template>
+            <div
+                v-else
+                class="text-center"
+            >
+                <Button @click="drawerOpen = !drawerOpen"> Create your first player! </Button>
+
+                <Drawer
+                    v-model:visible="drawerOpen"
+                    header="Create player"
+                    position="right"
+                    class="!w-full md:!w-[40rem]"
+                >
+                    <CreatePlayerForm />
+                </Drawer>
+            </div>
         </template>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { usePlayers } from '../composables/usePlayers'
+import CreatePlayerForm from '../components/CreatePlayerForm.vue'
 
 const { listLoading, getUserPlayers, list } = usePlayers()
+
+const drawerOpen = ref(false)
 
 onMounted(() => {
     getUserPlayers()
