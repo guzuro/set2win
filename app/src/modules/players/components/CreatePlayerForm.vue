d4b55d929f1b7bbf1868674319abff02fa102bb4
guzuro <guzurogmn@gmail.com>
2025-04-25 22:45:52 +0300

    started createplayerform

--- app/src/modules/players/components/CreatePlayerForm.vue
+++ app/src/modules/players/components/CreatePlayerForm.vue
@@ -3,14 +3,37 @@
         <FormBuilder
             :form="createPlayerForm"
             :resolver="createPlayer"
-        />
+        >
+            <template #country="{ field, error }">
+                <SelectInput
+                    v-if="field.type === 'Select'"
+                    :name="field.model"
+                    :label="field.label"
+                    :error="error"
+                    v-bind="field.attrs"
+                >
+                    <template #option="slotProps">
+                        <div class="flex items-center gap-2">
+                            <img
+                                :alt="slotProps.option.label"
+                                :src="`https://flags.fmcdn.net/data/flags/mini/${slotProps.option.value.toLowerCase()}.png`"
+                                style="width: 18px"
+                            />
+                            <div>{{ slotProps.option.label }}</div>
+                        </div>
+                    </template>
+                </SelectInput>
+            </template>
+        </FormBuilder>
     </div>
 </template>
 
 <script setup lang="ts">
 import FormBuilder from '@/shared/components/FormBuilder/FormBuilder.vue'
 import type { FormBuilderData } from '@/shared/components/FormBuilder/types'
+import { countryList } from '@/shared/includes/countriesList'
 import type { FormResolverOptions } from '@primevue/forms'
+import SelectInput from '@/shared/components/controls/SelectInput.vue'
 
 const createPlayerForm: FormBuilderData<any> = {
     model: {
@@ -22,25 +45,27 @@ const createPlayerForm: FormBuilderData<any> = {
     },
     schema: [
         {
-          type: 'Container',
-          fields: [
-            {
+            type: 'Simple',
+            field: {
                 type: 'Text',
-                label: '111',
-                model: 'fullName'
+                model: 'fullName',
+                label: 'Player name',
             },
-            {
-                type: 'Text',
-                label: 'Country',
-                model: 'country'
-            }
-          ]  
         },
         {
-            type: 'Text',
-            label: 'hand',
-            model: 'hand    '
-        }
+            type: 'Simple',
+            field: {
+                type: 'Select',
+                model: 'country',
+                label: 'Country',
+                attrs: {
+                    virtualScrollerOptions: { itemSize: 38 },
+                    options: Object.entries(countryList).map(([k, v]) => ({ label: v, value: k })),
+                    optionLabel: 'label',
+                    optionValue: 'value',
+                },
+            },
+        },
     ],
     submit: { label: 'Create player' },
 }
