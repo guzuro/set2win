bdcee369bf5b485fbbe3d50b9ad2d795f7cb6574
guzuro <guzurogmn@gmail.com>
2025-05-13 13:22:20 +0300

    removed primevue

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,20 +3,11 @@ import './assets/tailwind.css'
 import './assets/main.scss'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
-import 'primeicons/primeicons.css'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
 const pinia = createPinia()
 
-app.use(router)
-    .use(pinia)
-    .use(PrimeVue, {
-        theme: {
-            preset: Aura,
-        },
-    })
+app.use(router).use(pinia)
 
 app.mount('#app')
