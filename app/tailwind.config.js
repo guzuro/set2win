3c8726e07eb0dc08ec6ec42d6f88d55dfa70e6e5
guzuro <guzurogmn@gmail.com>
2025-08-05 21:08:43 +0300

    sync tw screens with antd grid

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,6 +3,15 @@ export default {
     content: ['./src/**/*.{html,js,ts,vue}'],
     theme: {
         extend: {},
+        screens: { //https://antdv.com/components/layout#breakpoint-width
+            xs: '480px',
+            sm: '576px',
+            md: '768px',
+            lg: '992px',
+            xl: '1200px',
+            xxl: '1600px',
+            xxxl: '2000px',
+        }
     },
     plugins: [require('@tailwindcss/container-queries')],
 }
