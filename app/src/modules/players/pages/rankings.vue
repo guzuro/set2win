9902785f883e1301099a65e64fc69cf43ce81d35
guzuro <guzurogmn@gmail.com>
2025-08-05 23:31:22 +0300

    started rankings table

--- app/src/modules/players/pages/rankings.vue
+++ app/src/modules/players/pages/rankings.vue
@@ -9,27 +9,64 @@
             v-if="rankingsLoading"
             class="w-full"
         />
-        <ul v-else>
-            <li
-                v-for="player in rankingsData?.players"
-                :key="player.id"
-            >
-                <ACard size="small">
-                    {{ player }}
+        <ATable
+            v-else
+            :dataSource="rankingsData?.players ?? []"
+            :columns="columns"
+        >
+            <template #bodyCell="{ column, record }">
+                <template v-if="column.key === 'player'">
+                    <div class="flex items-center gap-2">
+                        <div class="relative">
+                            <Avatar
+                                shape="circle"
+                                size="large"
+                                :src="getPlayerAvatarUrl(record.avatarUrl)"
+                            />
 
-                    <Avatar :src="`http://localhost:3000/${player.avatarUrl}`" />
-                </ACard>
-            </li>
-        </ul>
+                            <CountryFlag
+                                class="absolute -top-1 -right-2"
+                                :country="record.country"
+                                :has-title="false"
+                            />
+                        </div>
+
+                        <b>
+                            {{ record.fullName }}
+                        </b>
+                    </div>
+                </template>
+            </template>
+        </ATable>
     </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { usePlayers } from '../composables/usePlayers'
+import type { ColumnsType } from 'ant-design-vue/es/table'
 import { Avatar } from 'ant-design-vue'
+import CountryFlag from '@/shared/components/controls/components/CountryFlag.vue'
+import { getPlayerAvatarUrl } from '../includes/logic'
 
 const { getRankings, rankingsData, rankingsLoading } = usePlayers()
+
+const columns: ColumnsType = [
+    {
+        title: 'Rank',
+        dataIndex: 'rating',
+        key: 'rank',
+    },
+    {
+        title: 'Player',
+        key: 'player',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        key: 'age',
+    },
+]
 
 onMounted(() => {
     getRankings()
