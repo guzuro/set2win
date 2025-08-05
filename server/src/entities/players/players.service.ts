137b67bb0bb1572b89ce5008c4f579ff7eeebb16
guzuro <guzurogmn@gmail.com>
2025-08-05 21:07:51 +0300

    path to avatarurl fix

--- server/src/entities/players/players.service.ts
+++ server/src/entities/players/players.service.ts
@@ -15,7 +15,7 @@ class PlayersService {
     async addPlayer(player: PlayerCreateDto) {
         try {
             if (!player.avatarUrl) {
-                const imgType = player.sex === 'male' ? 'default-men.jpg' : 'default-women.jpg'
+                const imgType = player.sex === 'male' ? 'default-male.jpg' : 'default-female.jpg'
 
                 player.avatarUrl = `${AVATAR_PUBLIC_URL}/${imgType}`
             }
